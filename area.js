30cf7d6a306d83fc5f9d541a0fea1862700557e5
Manish Kumar <manish.mk0195@gmail.com>
2021-09-23 16:12:44 +0530

    first commit

---
+++ area.js
@@ -0,0 +1,16 @@
+const sides = document.querySelectorAll('.side-input')
+const areaBtn = document.querySelector('#area-btn');
+const outputE1 = document.querySelector('#output')
+
+function calculateProductOfSides(a, b){
+    const productofSides = a*b;
+    return productofSides;
+}
+
+function calculateArea(){
+    const productofSides = calculateProductOfSides(Number(sides[0].value), Number(sides[1].value));
+    const area = parseFloat(productofSides/2);
+    outputE1.innerText = "area is : " + area;
+}
+
+areaBtn.addEventListener("click", calculateArea);
