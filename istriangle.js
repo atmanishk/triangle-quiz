c9d690aec7f648d45c27013de01308eada1c086b
Manish Kumar <manish.mk0195@gmail.com>
2021-09-25 11:42:55 +0530

    review changes

--- istriangle.js
+++ istriangle.js
@@ -2,18 +2,38 @@ const inputs = document.querySelectorAll('.angle-input')
 const isTriangleBtn = document.querySelector('#is-triangle-btn');
 const outputE1 = document.querySelector('#output');
 
-function calculateSumOfAngles(angle1, angle2, angle3){
+function calculateSumOfAngles(angle1, angle2, angle3) {
     const sumOfAngles = angle1 + angle2 + angle3;
     return sumOfAngles;
 }
 
-function isTriangle(){
-    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
-    if(sumOfAngles === 180){
-        outputE1.innerText = "Yay, The angles form a triangle";
-    }else{
-        outputE1.innerText = "Noooo! These angles don't form a triangle";
+function calculateHandler() {
+    var angle1 = Number(inputs[0].value);
+    var angle2 = Number(inputs[1].value);
+    var angle3 = Number(inputs[2].value);
+
+    isTriangle(angle1, angle2, angle3);
+}
+
+function isTriangle(angle1, angle2, angle3) {
+    if (angle1 && angle2 && angle3) {
+        if (angle1 > 0 && angle2 > 0 && angle3 > 0) {
+            if (angle1 < 179 && angle2 < 179 && angle3 < 179) {
+                const sumOfAngles = calculateSumOfAngles(angle1, angle2, angle3);
+                if (sumOfAngles === 180) {
+                    outputE1.innerText = "Yay, The angles form a triangle";
+                } else {
+                    outputE1.innerText = "Noooo! These angles don't form a triangle";
+                }
+            } else {
+                outputE1.innerText = "Angle must be less than 179 degrees";
+            }
+        } else {
+            outputE1.innerText = "Angle of a triangle must be greater than zero degrees";
+        }
+    } else {
+        outputE1.innerText = "Please enter all the values";
     }
 }
 
-isTriangleBtn.addEventListener("click", isTriangle)
+isTriangleBtn.addEventListener("click", calculateHandler)
